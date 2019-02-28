097e5d701edf8d092a0480ffb98bd6d4377d371f
dpayton23 <darien.payton71@gmail.com>
2019-02-28 16:09:23 -0500

    added Student constructor function and comments to label all constructor functions

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,4 +1,5 @@
 // CODE here for your Lambda Classes
+//Person constructor
 class Person {
     constructor(info) {
         this.name = info.name;
@@ -11,6 +12,7 @@ class Person {
     };
 }
 
+//Instructor constructor
 class Instructor extends Person {
     constructor(attrs) {
         super(attrs);
@@ -23,5 +25,26 @@ class Instructor extends Person {
     };
     grade(student, subject) {
         console.log(`${student.name} receives a perfect score on ${subject}.`)
+    };
+}
+
+//Student constructor
+class Student extends Person {
+    constructor(studies) {
+        super(studies);
+        this.previousBackground = studies.previousBackground;
+        this.className = studies.className;
+        this.favSubjects = studies.favSubjects;
+    }
+    listsSubjects(){
+        for(let i = 0; i < this.favSubjects.length; i++) {
+            console.log(this.favSubjects[i])
+        };
+    };
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}.`);
+    };
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`)
     };
 }
