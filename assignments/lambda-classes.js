9ceda903fa9ccd7475e34f7f9c960539fe856c78
dpayton23 <darien.payton71@gmail.com>
2019-02-28 16:01:39 -0500

    created Instructor constructor function

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,18 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}.`
     };
 }
+
+class Instructor extends Person {
+    constructor(attrs) {
+        super(attrs);
+        this.specialty = attrs.specialty;
+        this.favLanguage = attrs.favLanguage;
+        this.catchPhrase = attrs.catchPhrase;
+    }
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}.`)
+    };
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}.`)
+    };
+}
